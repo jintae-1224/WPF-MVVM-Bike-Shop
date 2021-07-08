f99681adff53446d540764ed6da623b59dd4454d
Jintae <teaminnoper@gmail.com>
2021-07-08 14:41:12 +0900

    Finish

---
+++ MVVM2/MVVM2/Menu.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVM2
+{
+    /// <summary>
+    /// Menu.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                new Uri("/Contact.xaml", UriKind.Relative));        // 버튼이 클릭되었을때 Contact.xaml페이지로 이동하도록 코드추가
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                new Uri("/Discussion.xaml", UriKind.Relative));        // 버튼이 클릭되었을때 Discussion.xaml페이지로 이동하도록 코드추가
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                          new Uri("/ProductsManagement.xaml", UriKind.Relative));        // 버튼이 클릭되었을때 ProductsManagement.xaml페이지로 이동하도록 코드추가
+        }
+    }
+}
